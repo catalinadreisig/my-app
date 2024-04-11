e4785f74400ca05a59c9aa81f6ccd428d61dec0e
catalinadreisig <catalinadreisig@yahoo.dk>
2024-04-11 10:04:58 +0200

    hej

--- src/app/henry/pages.jsx
+++ src/app/henry/pages.jsx
@@ -1,16 +1,15 @@
-async function getHenry{} {
-    const res = await fetch  ("https://nice-dogs.vercel.app/")
-return
+async function getHenry() {
+  const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
+  return await res.json();
 }
-export default function HenryPage() {
-    const data = await getHenry()
-    console.log(data);
-        
-    
+
+export default async function HenryPage() {
+  const data = await getHenry();
+  console.log(data);
+
   return (
     <main>
-      <h1>(data.name)</h1>
-    
+      <h1>{data.name}</h1>
     </main>
   );
 }
